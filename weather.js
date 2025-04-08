6c579e4fa796a0fa119b26c1e31bf6bfe1d22a7b
Abhishek <abhishekkumardeshwal@gmail.com>
2025-04-09 00:12:43 +0530

    WeatherForecast

--- weather.js
+++
@@ -1,3 +0,0 @@
-const apiKey = "b438f16eb0c75fd741e0afc7fb8a53e3";
-        const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
-const url = `${apiUrl}${location}&appid=${apiKey}`;
