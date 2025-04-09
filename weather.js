7dd59d02d64f79396503ce18fa936a416c726d5e
Abhishek <abhishekkumardeshwal@gmail.com>
2025-04-10 00:12:09 +0530

    updated-app

--- weather.js
+++ weather.js
@@ -1,3 +1,28 @@
 const apiKey = "b438f16eb0c75fd741e0afc7fb8a53e3";
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
-const url = `${apiUrl}${location}&appid=${apiKey}`;
+const url = `${apiUrl}${'canada'}&appid=${apiKey}`;
+
+const searchBox = document.querySelector('#search input');
+const searchBtn = document.querySelector('#search button');
+const icon = document.querySelector('#weather-icon')
+
+async function WeatherForecast(city){
+    const res = await fetch(apiUrl + city + `&appid=${apiKey}`);
+    const data = await res.json();
+    
+
+    console.log(data);
+    document.querySelector('#city-name').innerHTML = data.name;
+    document.querySelector('#temperature').innerHTML = data.main.temp + "<sup>o</sup>c";
+    document.querySelector('#humidity').innerHTML = data.main.humidity + "%";
+    document.querySelector('#wind').innerHTML = data.wind.speed + "km/h";
+
+    // if(data.weather[0].main == "clouds"){
+    //     icon.src = "https://www.svgrepo.com/show/339074/cloudy-hazy.svg"
+    // }
+}
+
+searchBtn.addEventListener("click", () => {
+    WeatherForecast(searchBox.value);
+})
+
